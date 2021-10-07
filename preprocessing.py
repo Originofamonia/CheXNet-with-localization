c416fae3e76a373badad229a5de87142844e7259
Edward <anqiyuanms@hotmail.com>
2021-10-07 16:38:37 -0500

    merge transform img and label function

--- preprocessing.py
+++ preprocessing.py
@@ -25,7 +25,7 @@ def main():
     data_path = '/home/qiyuan/2021summer/CheXNet-with-localization/data/'  # output folder for preprocessed data
 
     # load data
-    meta_data = pd.read_csv(data_entry_path)
+    data_entry = pd.read_csv(data_entry_path)
     bbox_list = pd.read_csv(bbox_list_path)  # weak supervision not used?
     with open(train_txt_path, "r") as f:
         train_list = [i.strip() for i in f.readlines()]
@@ -36,39 +36,15 @@ def main():
     # process label
     print("Start preprocessing:")
     print(f"Training examples: {len(train_list)}")
-    train_x, train_y = [], []
-    pbar = tqdm(train_list)
-    for i, train_id in enumerate(pbar):
-        image_path = os.path.join(image_folder_path, train_list[i])
-        img = imageio.imread(image_path)
-        # there are some images with shape (1024,1024,4) in training set
-        if img.shape != (1024, 1024):
-            img = img[:, :, 0]
-        img_resized = skimage.transform.resize(img, (256, 256))
-        # or use img[::4] here
-        train_x.append((np.array(img_resized) / 255).reshape(256, 256, 1))
-        # train labels
-        train_y.append(get_labels(train_id, meta_data))
-        if i % 3000 == 0:
-            pbar.set_description(f'train_list: {i}/{len(train_list)}')
+    train_x, train_y = transform_img_label(image_folder_path, data_entry,
+                                           train_list)
 
     train_x = np.array(train_x)
     np.save(os.path.join(data_path, "train_x_small.npy"), train_x)
 
-    test_x, test_y = [], []
     print(f"Test examples: {len(test_list)}")
-    pbar = tqdm(test_list)
-    for i, test_id in enumerate(pbar):
-        image_path = os.path.join(image_folder_path, test_list[i])
-        img = imageio.imread(image_path)
-        if img.shape != (1024, 1024):
-            img = img[:, :, 0]
-        img_resized = skimage.transform.resize(img, (256, 256))
-        test_x.append((np.array(img_resized) / 255).reshape(256, 256, 1))
-        # Test labels
-        test_y.append(get_labels(test_id, meta_data))
-        if i % 3000 == 0:
-            pbar.set_description(f'test_list: {i}/{len(test_list)}')
+    test_x, test_y = transform_img_label(image_folder_path, data_entry,
+                                         test_list)
 
     test_x = np.array(test_x)
     np.save(os.path.join(data_path, "test_x_small.npy"), test_x)
@@ -88,6 +64,26 @@ def main():
         pickle.dump(test_y_onehot, f)
     with open(data_path + "/binarizer.pkl", "wb") as f:
         pickle.dump(binarizer, f)
+
+
+def transform_img_label(image_folder_path, df, train_list):
+    train_x, train_y = [], []
+    pbar = tqdm(train_list)
+    for i, img_id in enumerate(pbar):
+        image_path = os.path.join(image_folder_path, img_id)
+        img = imageio.imread(image_path)
+        # there are some images with shape (1024,1024,4) in training set
+        if img.shape != (1024, 1024):
+            img = img[:, :, 0]
+        img_resized = skimage.transform.resize(img, (256, 256))
+        # or use img[::4] here
+        train_x.append((np.array(img_resized) / 255).reshape(256, 256, 1))
+        # train labels
+        train_y.append(get_labels(img_id, df))
+        if i % 3000 == 0:
+            pbar.set_description(f'train or test list: {i}/{len(train_list)}')
+
+    return train_x, train_y
 
 
 if __name__ == '__main__':
