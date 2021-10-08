a6590db5f528de2264d3fea7d9fbfc23e69e9bfa
Edward <anqiyuanms@hotmail.com>
2021-10-08 15:12:47 -0500

    target_layer="module.densenet121.features.denseblock4.denselayer16.conv.2")

--- localization.py
+++ localization.py
@@ -223,7 +223,7 @@ def main():
         for activate_class in activate_classes:
             gcam.backward(idx=activate_class)
             output = gcam.generate(
-                target_layer="module.densenet121.features.denseblock4.denselayer16.conv2")
+                target_layer="module.densenet121.features.denseblock4.denselayer16.conv.2")
             # this output is heatmap
             if np.sum(np.isnan(output)) > 0:
                 print("fxxx nan")
